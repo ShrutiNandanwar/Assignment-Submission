7a0c94e4b2b0601e55b28bf8ded351c60dda9b8d
ShrutiNandanwar <73781579+ShrutiNandanwar@users.noreply.github.com>
2020-11-01 22:44:13 +0530

    Day 2 Assignment Submission

---
+++ Day2/Employee.java
@@ -0,0 +1,13 @@
+
+class Employee {
+
+public String name;
+public int age;
+public String city;
+
+public void display() {
+	System.out.println("The name is "+name);
+	System.out.println("The age is "+age);
+	System.out.println("The city is "+city);
+}
+}
