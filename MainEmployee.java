43561b4591239db7b8c6bd717eab94aa6da78dc2
ShrutiNandanwar <73781579+ShrutiNandanwar@users.noreply.github.com>
2020-11-01 18:55:54 +0530

    Day 2  Assignment Submitted

---
+++ MainEmployee.java
@@ -0,0 +1,18 @@
+
+public class MainEmployee {
+
+	public static void main(String[] args) {
+		Employee e1=new Employee();
+		e1.name="Shruti";
+		e1.age=24;
+		e1.city="Nagpur";
+		e1.display();
+		
+		Employee e2=new Employee();
+		e2.name="Amruta";
+		e2.age=24;
+		e2.city="Mumbai";
+		e2.display();
+	}
+
+}
